a64093c7f81194c580f347440bdac76fadeadbac
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-01-16 07:50:22 +0700

    versi 1.0
    Menampilkan Tulisan dan HTML

---
+++ Youtube/lat01.php
@@ -0,0 +1,10 @@
+<?php 
+echo "belajar php";
+echo "<br>";
+echo 'saya sisswi SMK';
+echo '<h1 style="background-color:violet">Belajar PHP itu mudah</h1>
+    <p style="background-color:grey">saya suka belajar program<p>
+';
+echo 'saya belajar'.' PHP'.'<br>';
+echo 2019 + 5;
+?>
