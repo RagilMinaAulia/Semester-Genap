8b25374738d206c3bb8578a8e3557d37aabcec6d
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-01-18 13:43:51 +0700

    versi 1.3
    Operator PHP

---
+++ Youtube/lat04.php
@@ -0,0 +1,48 @@
+<?php 
+//OPERATOR MATEMATIKA
+$a=2;
+$b=2;
+$c= $a+$b;
+echo $c.'<br>';
+
+$c= $a-$b;
+echo $c.'<br>';
+
+$c= $a*$b;
+echo $c.'<br>';
+
+$c= $a/$b;
+echo $c.'<br>';
+echo round($c).'<br>';
+echo floor($c).'<br>';
+
+$c = $a%$b;
+echo $c.'<br>';
+
+//OPERATOR LOGIKA
+$c= $a<$b;
+echo $c;
+
+$c= $a>$b;
+echo $c;
+
+$c= $a==$b;
+echo $c;
+
+$c= $a!=$b;
+echo $c.'<br>';
+
+//INCREMENT
+// $a++;
+// echo $a.'<br>';
+
+$a--;
+echo $a.'<br>';
+
+//OPERATOR STRING
+$kata= 'sura';
+$kota= 'baya';
+$hasil= $kata.$kota;
+$hasil .= ' kota pahlawan';
+echo $hasil;
+?>
