ac05987c6c8b259ef224c94f81274970e1b67892
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-01-23 07:26:36 +0700

    versi 2.1
    GET

--- Youtube/nav.php
+++ Youtube/nav.php
@@ -1,17 +1,15 @@
 <nav>
     <ul>
-        <li>kontak</li>
-        <li>sejarah</li>
-        <li>jurusan</li>
+        <li><a href="?menu=kontak">Kontak</a></li>
+        <li><a href="?menu=sejarah">Sejarah</a></li>
+        <li><a href="?menu=jurusan">Jurusan</a></li>
     </ul>
 </nav>
 
 <?php 
-    if (isset($_POST['kirim'])) {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        echo $email;
-        echo '<br>';
-        echo $password;
+    if (isset($_GET['menu'])) {
+        $menu= $_GET['menu'];
+        require_once $menu.'.php';
     }
+
 ?>
