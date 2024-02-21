650a71d01439b2445bf579f3e7bb6edf6a2bcc17
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-02-21 18:32:22 +0700

    Menambah Dan Menghapus Isi Cart

--- Youtube/restoran/home/beli.php
+++ Youtube/restoran/home/beli.php
@@ -5,6 +5,20 @@
         unset($_SESSION['_'.$id]);
     }
 
+    if (isset($_GET['tambah'])) {
+        $id= $_GET['tambah'];
+        $_SESSION['_'.$id]++;
+    }
+
+    if (isset($_GET['kurang'])) {
+        $id= $_GET['kurang'];
+        $_SESSION['_'.$id]--;
+        
+        if ($_SESSION['_'.$id]==0) {
+            unset($_SESSION['_'.$id]);
+        }
+    }
+
     if (!isset($_SESSION['pelanggan'])) {
         header("location:?f=home&m=login");
     }else {
@@ -27,8 +41,9 @@
 
     function keranjang(){
         global $db;
+        $total= 0;
         echo '
-            <table class="table table-bordered w-50">
+            <table class="table table-bordered w-70">
                 <tr>
                     <th>Menu</th>
                     <th>Harga</th>
@@ -48,13 +63,21 @@
                     echo '<tr>';
                     echo '<td>'.$r['menu'].'</td>';
                     echo '<td>'.$r['harga'].'</td>';
-                    echo '<td>'.$value.'</td>';
+                    echo '<td><a href="?f=home&m=beli&tambah='.$r['idmenu'].'">[+] </a> 
+                        &nbsp &nbsp'.$value.'&nbsp &nbsp 
+                        <a href="?f=home&m=beli&kurang='.$r['idmenu'].'">[-] </a>
+                    </td>';
                     echo '<td>'.$r['harga']*$value.'</td>';
                     echo '<td><a href="?f=home&m=beli&hapus='.$r['idmenu'].'">Hapus</a></td>';
                     echo '</tr>';
+                    $total= $total+($value*$r['harga']);
                 }
             }
         }
+        echo '<tr>
+            <td colspan=4><h3>Total semua: </h3></td>
+            <td><h3>'.$total.'</h3></td>
+        </tr>';
         echo '</table>';
     }
 ?>
